2fe84fceefb318de1636123450a7e1865d4acee4
sk <content.type.text.net@gmail.com>
2022-11-04 22:38:43 +0900

    example.com

--- webpage/example.com/example.com.ts
+++ webpage/example.com/example.com.ts
@@ -16,6 +16,36 @@ export default class ExampleCom implements webpage.SettingFactory {
 			],
 			level: 0,
 			language: 'ja',
+			"path": {
+				"/": {
+					// セレクタに対する処理
+					"query": [
+						{
+							"selector": {
+								"value": "body p:nth-child(2)"
+							},
+							// 対象要素の textContent
+							"text": {
+								"replace": {
+									"mode": "normal",
+									"value": "このドメインは文書例として使用するものです。 このドメインは文書に掲載する場合、事前の調整や許可を得ずに使用可能です。"
+								}
+							}
+						},
+						{
+							"selector": {
+								"value": "body p:nth-child(3) a"
+							},
+							"text": {
+								"replace": {
+									"mode": "normal",
+									"value": "詳しくはこちら..."
+								}
+							}
+						}
+					]
+				}
+			}
 		};
 
 		return result;
