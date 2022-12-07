66b0b742d61a313fa013c624958c56ad7e51489f
sk <content.type.text.net@gmail.com>
2022-12-07 17:51:19 +0900

    ひな形のみ追加したけどまだいっぱいあるなぁ

--- webpage/github.com/github-actions.ts
+++ webpage/github.com/github-actions.ts
@@ -1,19 +1,22 @@
 //import * as setting from '../../page-translation-dictionary/source/scripts/extension/setting';
 import * as webpage from '../webpage';
-import { CommonQuery } from './github-common-query'
+import { CommonQuery } from './github-common-query';
+//import { CommonText } from './github-common-text';
 
 /**
- * ファイル表示
+ * アクション
  */
 export default function get(): webpage.PathPair {
 	return {
-		pattern: /^\/([a-zA-Z0-9_-]+)\/([\.a-zA-Z0-9_-]+)\/blob/,
+		pattern: /^\/([a-zA-Z0-9_-]+)\/([\.a-zA-Z0-9_-]+)\/actions(\/(workflows|caches))?/,
 		setting: {
 			query: [
+				//#region
+				//#endregion
 			],
 			import: [
 				...Object.keys(CommonQuery).filter(i => !(/^\d+$/.test(i))),
 			]
-		},
+		}
 	};
 }
--- webpage/github.com/github-settings.ts
+++ webpage/github.com/github-settings.ts
@@ -1,19 +1,21 @@
 //import * as setting from '../../page-translation-dictionary/source/scripts/extension/setting';
 import * as webpage from '../webpage';
-import { CommonQuery } from './github-common-query'
+import { CommonQuery } from './github-common-query';
 
 /**
- * ファイル表示
+ * 設定
  */
 export default function get(): webpage.PathPair {
 	return {
-		pattern: /^\/([a-zA-Z0-9_-]+)\/([\.a-zA-Z0-9_-]+)\/blob/,
+		pattern: /^\/([a-zA-Z0-9_-]+)\/([\.a-zA-Z0-9_-]+)\/settings/,
 		setting: {
 			query: [
+				//#region
+				//#endregion
 			],
 			import: [
 				...Object.keys(CommonQuery).filter(i => !(/^\d+$/.test(i))),
 			]
-		},
+		}
 	};
 }
